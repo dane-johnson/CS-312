c09d74c1d4827dd1beac19e3d4743a1da8e40874
dane-johnson <dane_johnson@ymail.com>
2016-02-12 11:38:16 -0600

    Optimized code for maximum speed

--- Program 1/matAverager.cpp
+++ Program 1/matAverager.cpp
@@ -6,18 +6,21 @@
 #include <omp.h>
 #include <cstdlib>
 #include <sstream>
-#include <list>
 #include <string>
 
 using namespace std;
 
 typedef unsigned int ** val;
+typedef double ** sol;
 
 struct ordered_pair
 {
 	int x;
 	int y;
-	ordered_pair(){}
+	ordered_pair(){
+		x = 1;
+		y = 1;
+	}
 	ordered_pair(int init_x, int init_y)
 	{
 		x = init_x;
@@ -25,8 +28,13 @@ struct ordered_pair
 	}
 };
 
-double findmaxavg(val, const int&, const int&, list<ordered_pair>&);
-double findlocalavg(val, const int&, const int&, const int&, const int&);
+double findmaxavg(val, const int&, const int&, ordered_pair&);
+void populateSolCenter(val, const int&, const int&, sol);
+void populateSolTopEdge(val, const int&, const int&, sol);
+void populateSolBottomEdge(val, const int&, const int&, sol);
+void populateSolLeftEdge(val, const int&, const int&, sol);
+void populateSolRightEdge(val, const int&, const int&, sol);
+//double findlocalavg(val, const int&, const int&, const int&, const int&);
 
 // a class to get more accurate time
 
@@ -209,111 +217,207 @@ int main( int argc, char* argv[] )
 	S1.start();
 	
 	double avg;
-	list<ordered_pair> pairs;
+	ordered_pair pair;
 	
-	avg = findmaxavg(data, rows, cols, pairs);
+	avg = findmaxavg(data, rows, cols, pair);
 	
 	S1.stop();
 	
 	// print out the max value here
 	
 	cout << "largest average: " << avg << endl;
-	cout << "found at cells: ";
-	while(!pairs.empty())
-	{
-		ordered_pair curr = pairs.front();
-		cout << "(" << curr.x << ", " << curr.y <<")  ";
-		pairs.pop_front();
-	}
-	cout << endl;
+	cout << "found at cell: (" << pair.x << ", " << pair.y << ")" << endl ;
+	// //while(!pairs.empty())
+	// //{
+		// ordered_pair curr = pairs.front();
+		// cout << "(" << curr.x << ", " << curr.y <<")  ";
+		// pairs.pop_front();
+	// }
+	//cout << endl;
 	cerr << "elapsed time: " << S1.getTime( ) << endl;
 }
 
 //finds max average iteratively
-double findmaxavg(val data, const int& rows, const int& cols, list<ordered_pair>& pairs)
+double findmaxavg(val data, const int& rows, const int& cols, ordered_pair& pair)
 {
-	double max = findlocalavg(data, 0, 0, rows, cols); //set first value to max
-	pairs.push_front(ordered_pair(0, 0));
+	//double max = findlocalavg(data, 0, 0, rows, cols); //set first value to max
+	// pairs.push_front(ordered_pair(0, 0));
 	
+	// #pragma omp parallel for
+	// for(int i = 0; i < rows; i++)
+	// {
+		// for(int j = 0; j < cols; j++)
+		// {
+			// double test = findlocalavg(data, i, j, rows, cols); //get the local average
+			// if(test > max)
+			// {
+				// #pragma omp critical
+				// {
+					// if(test > max)
+					// {
+						// max = test; // if it's better, replace it
+						// pairs.clear();
+						// pairs.push_front(ordered_pair(i, j));
+					// }
+				// }
+			// } 
+		// }
+	// }
+	// #pragma omp barrier
+	
+	sol solution = new double* [rows];
+	#pragma omp parallel for
+	for (int i = 0; i < rows; i++)
+	{
+		solution[i] = new double[cols];
+	}
+	
+	populateSolCenter(data, rows, cols, solution);
+	populateSolRightEdge(data, rows, cols, solution);
+	populateSolLeftEdge(data, rows, cols, solution);
+	populateSolTopEdge(data, rows, cols, solution);
+	populateSolBottomEdge(data, rows, cols, solution);
+	
+	solution[0][0] = double(data[0][0] + data[0][1] + data[1][0] + data[1][1]) / 4;
+	solution[rows - 1][0] = double(data[rows - 1][0] + data[rows - 1][1] + data[rows - 2][0] + data[rows - 2][1]) / 4;
+	solution[0][cols - 1] = double(data[0][cols - 1] + data[1][cols - 1] + data[0][cols - 2] + data[1][cols - 2]) / 4;
+	solution[rows - 1][cols - 1] = double(data[rows - 1][cols - 1] + data[rows - 1][cols - 2] + data[rows - 2][cols - 1] + data[rows - 2][cols - 2]) / 4;
+	
+	double maximum = 0.0;
 	#pragma omp parallel for
 	for(int i = 0; i < rows; i++)
 	{
 		for(int j = 0; j < cols; j++)
 		{
-			double test = findlocalavg(data, i, j, rows, cols); //get the local average
-			if(test > max)
+			if(solution[i][j] > maximum)
 			{
 				#pragma omp critical
 				{
-					if(test > max)
+					if(solution[i][j] > maximum)
 					{
-						max = test; // if it's better, replace it
-						pairs.clear();
-						pairs.push_front(ordered_pair(i, j));
+						maximum = solution[i][j];
+						pair.x = i;
+						pair.y = j;
 					}
 				}
-			} 
-			else if (test == max)
-			{
-				#pragma omp critical
-				{
-					pairs.push_back(ordered_pair(i, j));
-				}
-			}				//They are the same, add this to the pairs
+			}
 		}
 	}
-	#pragma omp barrier
 	
-	return max;
+	return maximum;
 }
 
-//
-double findlocalavg(val data, const int& i, const int& j, const int& rows, const int& cols)
+void populateSolCenter(val data, const int& rows, const int& cols, sol solution)
 {
-	int nvals = 1, total = data[i][j]; //We have at least one value
-	bool isTop = (j == 0), isBottom = (j + 1 == cols), isLeftEdge = (i == 0), isRightEdge = (i + 1 == rows);
-	if(!isLeftEdge) //We're clear to the left
+	#pragma omp parallel for
+	for(int i = 1; i < rows - 1; i++)
 	{
-		nvals++;
-		total += data[i - 1][j];
-		if(!isTop)//Top left clear
+		for(int j = 1; j < cols - 1; j++)
 		{
-			nvals++;
-			total += data[i - 1][j - 1];
-		}
-		if(!isBottom)//Bottom left clear
-		{
-			nvals++;
-			total += data[i - 1][j + 1];
+			solution[i][j] = double(
+				data[i - 1][j - 1] + data[i][j - 1] + data[i + 1][j - 1] +
+				data[i - 1][j]     + data[i][j]     + data[i + 1][j] +
+				data[i - 1][j + 1] + data[i][j + 1] + data[i + 1][j + 1]
+				) / 9;
 		}
 	}
-	if(!isRightEdge) //We're clear to the right
-	{
-		nvals++;
-		total += data[i + 1][j];
-		if(!isTop)//Top right clear
-		{
-			nvals++;
-			total += data[i + 1][j - 1];
-		}
-		if(!isBottom)//Bottom right clear
-		{
-			nvals++;
-			total += data[i + 1][j + 1];
-		}
-	}
-	if(!isTop) //Top clear
-	{
-		nvals++;
-		total += data[i][j - 1];
-	}
-	if(!isBottom)
-	{
-		nvals++;
-		total += data[i][j + 1];
-	}
-	
-	return double(total) / nvals;
 }
+
+void populateSolTopEdge(val data, const int& rows, const int& cols, sol solution)
+{
+	#pragma omp parallel for
+	for(int i = 1; i < cols - 1; i++)
+	{
+		solution[0][i] = double(
+				data[0][i - 1] + data[0][i] + data[0][i + 1] +
+				data[1][i - 1] + data[1][i] + data[1][i + 1]
+			) / 6;
+			
+	}
+}
+
+void populateSolLeftEdge(val data, const int& rows, const int& cols, sol solution)
+{
+	#pragma omp parallel for
+	for(int i = 1; i < rows - 1; i++)
+	{
+		solution[i][0] = double(
+				data[i - 1][0] + data[i][0] + data[i + 1][0] +
+				data[i - 1][1] + data[1][1] + data[i + 1][1]
+			) / 6;
+	}
+}
+
+void populateSolBottomEdge(val data, const int& rows, const int& cols, sol solution)
+{
+	#pragma omp parallel for
+	for(int i = 1; i < cols - 1; i++)
+	{
+		solution[rows - 1][i] = double(
+				data[rows - 1][i - 1] + data[rows - 1][i] + data[rows - 1][i + 1] +
+				data[rows - 2][i - 1] + data[rows - 2][i] + data[rows - 2][i + 1]
+			) / 6;
+			
+	}
+}
+
+void populateSolRightEdge(val data, const int& rows, const int& cols, sol solution)
+{
+	#pragma omp parallel for
+	for(int i = 1; i < rows - 1; i++)
+	{
+		solution[i][cols - 1] = double(
+				data[i - 1][cols - 1] + data[i][cols - 1] + data[i + 1][cols - 1] +
+				data[i - 1][cols - 2] + data[1][cols - 2] + data[i + 1][cols - 2]
+			) / 6;
+	}
+}
+// double findlocalavg(val data, const int& i, const int& j, const int& rows, const int& cols)
+// {
+	// int nvals = 1, total = data[i][j]; //We have at least one value
+	// bool isTop = (j == 0), isBottom = (j + 1 == cols), isLeftEdge = (i == 0), isRightEdge = (i + 1 == rows);
+	// if(!isLeftEdge) //We're clear to the left
+	// {
+		// nvals++;
+		// total += data[i - 1][j];
+		// if(!isTop)//Top left clear
+		// {
+			// nvals++;
+			// total += data[i - 1][j - 1];
+		// }
+		// if(!isBottom)//Bottom left clear
+		// {
+			// nvals++;
+			// total += data[i - 1][j + 1];
+		// }
+	// }
+	// if(!isRightEdge) //We're clear to the right
+	// {
+		// nvals++;
+		// total += data[i + 1][j];
+		// if(!isTop)//Top right clear
+		// {
+			// nvals++;
+			// total += data[i + 1][j - 1];
+		// }
+		// if(!isBottom)//Bottom right clear
+		// {
+			// nvals++;
+			// total += data[i + 1][j + 1];
+		// }
+	// }
+	// if(!isTop) //Top clear
+	// {
+		// nvals++;
+		// total += data[i][j - 1];
+	// }
+	// if(!isBottom)
+	// {
+		// nvals++;
+		// total += data[i][j + 1];
+	// }
+	
+	// return double(total) / nvals;
+// }
 
 
