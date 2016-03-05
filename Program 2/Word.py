e7ed14daaa0646505b8521d33dab97c950e16a9d
Dane Johnson <dane_johnson@ymail.com>
2016-03-05 17:04:04 -0600

    Began work on program 2, added python module for frameword

---
+++ Program 2/Word.py
@@ -0,0 +1,18 @@
+class Word:
+	word = 0;
+	def __init__(self, bytes):
+		self.word += bytes[0] * 2**24;
+		self.word += bytes[1] * 2**16;
+		self.word += bytes[2] * 2**8;
+		self.word += bytes[3];
+
+class RType(Word):
+	def __getitem__(self, index):
+		return {
+			'op'  : ((self.word & 0b11111100000000000000000000000000) >> 26),
+			'rs'  : ((self.word & 0b00000011111000000000000000000000) >> 21),
+			'rt'  : ((self.word & 0b00000000000111110000000000000000) >> 16),
+			'rd'  : ((self.word & 0b00000000000000001111100000000000) >> 11),
+			'sa'  : ((self.word & 0b00000000000000000000011111000000) >> 6),
+			'func':  (self.word & 0b00000000000000000000000000111111)
+		}[index]
