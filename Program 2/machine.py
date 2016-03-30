ed3f9ff0242eb8b2caf355271620ca0f76d63a70
Dane Johnson <dane_johnson@ymail.com>
2016-03-29 22:31:01 -0500

    I broke it but on purpose like

--- Program 2/machine.py
+++ Program 2/machine.py
@@ -11,14 +11,27 @@ class Machine:
 	def loadInstructions(self, instructions):
 		self.instructions = instructions;
 	def execute(self, instruction):
-		if  ((instruction.getOp() & 0b100000) >> 5 == 0): 		#Invalid Instruction
-			print "Invalid Instruction Received"
-		elif((instruction.getOp() & 0b011111) == 0): 			#RType instruction
-			print "RType Instruction Received"
-		elif ((instruction.getOp() & 0b011110) == 0b000010): 	#JType
-			print "JType Instruction Received"
-		else: 													#IType
-			print "IType Instruction Received"
+		op = instruction['op']
+		if op == 0b100000 :# R Type, many cases
+			func = instruction['func']
+			if   func == 0b000000: return self.SLL(instruction)		# SLL or NOP
+			elif func == 0b000010: return self.SRL(instruction)		# SRL
+			elif func == 0b001000: return self.JR(instruction)		# JR
+			elif func == 0b100000: return self.ADD(instruction)		# ADD
+			elif func == 0b100010: return self.SUB(instruction)		# SUB
+			elif func == 0b100100: return self.AND(instruction)  # AND
+			elif func == 0b100101: return self.OR(instruction)		# OR
+			elif func == 0b001010: return self.MOVZ(instruction) # MOVZ
+			elif func == 0b001101: return self.BREAK(instruction)#BREAK
+			else :				   return self.INVALID(instruction)
+		elif op == 0b111100: return self.MUL(instruction)		#MUL
+		elif op == 0b100001: return self.BLTZ(instruction)		#BLTZ
+		elif op == 0b100010: return self.J(instruction)				#J
+		elif op == 0b100100: return self.BEQ(instruction)		#BEQ
+		elif op == 0b101000: return self.ADDI(instruction)		#ADDI
+		elif op == 0b100011: return self.LW(instruction) 		#LW
+		elif op == 0b101011: return self.SW(instruction)  		#SW
+		else:				 return self.INVALID(instruction)
 	def executeInstructions(self):
 		while(self.register[PC] < self.instructions[-1].addr):
 			i = self.findInstruction(self.register[PC])
