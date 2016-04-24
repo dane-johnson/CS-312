d34a5fd8f520ecc9efdabda09c00e8331a745f2f
Dane Johnson <dane_johnson@ymail.com>
2016-04-24 16:41:14 -0500

    updated output to work with current setup

--- Program 3/superscaler_sim/output.py
+++ Program 3/superscaler_sim/output.py
@@ -1,22 +1,33 @@
+from superscaler_sim import Word
 def output(machine, file): #file should be an open, write textually file
   out = ''
   def p(x=''): out += x + '\n' #quick helper function
+  def bin(w):
+    s = ''
+    for i in range(31, -1, -1):
+      s += str(w & (1 << i))
+    return s
   p('-' * 20)
   p('Cycle %d:' % machine.cycleCounter) #print the cycle
   p() #print a blank line
-  p('Pre-Issue Buffer:')
-  for i, k in enumerate(machine.preIssue.queue):
-    p(('\tEntry %d:\t' % i)+dissassemble(k.['instruction']))
-  p('Pre_ALU Queue:')
-  for i, k in enumerate(machine.preALU.queue):
-    p(('\tEntry %d:\t' % i)+dissassemble(k.['instruction']))
-  p('Post_ALU Queue:')
-  p(('\tEntry %d:\t' % 0)+dissassemble(machine.postAlu.entry['instruction']))
-  p('Pre_MEM Queue:')
-  for i, k in enumerate(machine.preMEM.queue):
-    p(('\tEntry %d:\t' % i)+dissassemble(k.['instruction']))
-  p('Post_MEM Queue:')
-  p(('\tEntry %d:\t' % 0)+dissassemble(machine.postMem.entry['instruction']))
+  if(len(machine.preIssue)):
+    p('Pre-Issue Buffer:')
+    for i, k in enumerate(machine.preIssue.queue):
+      p(('\tEntry %d:\t' % i)+dissassemble(k.['instruction']))
+  if(len(machine.preAlu)):
+    p('Pre_ALU Queue:')
+    for i, k in enumerate(machine.preALU.queue):
+      p(('\tEntry %d:\t' % i)+dissassemble(k.['instruction']))
+  if(len(machine.postAlu):
+    p('Post_ALU Queue:')
+    p(('\tEntry %d:\t' % 0)+dissassemble(machine.postAlu.entry['instruction']))
+  if(len(machine.preMem):
+    p('Pre_MEM Queue:')
+    for i, k in enumerate(machine.preMEM.queue):
+      p(('\tEntry %d:\t' % i)+dissassemble(k.['instruction']))
+  if(len(machine.postMem):
+    p('Post_MEM Queue:')
+    p(('\tEntry %d:\t' % 0)+dissassemble(machine.postMem.entry['instruction']))
   p() #print a blank line
   p('Registers:')
   for i, v in enumerate(machine.registers):
@@ -27,18 +38,22 @@ def output(machine, file): #file should be an open, write textually file
 			p()
   for i, k in enumerate(machine.cache.sets):
     p('\tSet %d: LRU=%d' % i, k.lru)
-    for i, k in enumerate(k.entries):
-      p() #idk how cache works
+    for j, l in enumerate(k.entries):
+      p('\tEntry %d: [(%d, %d, %d,)<%s,%s>]' % (j, l.v, l.d, l.tag, bin(l.words[0]), bin(l.words[1])))
+  p()
   dataAddr = findDataAddress(machine)
-  for i, instruction in enumerate(self.instructions[dataAddr:]):
+  for i, instruction in enumerate(machine.memory.instructions[dataAddr:]):
 		if(i % 8 == 0):
 			out += '%02d:' % instruction.addr
 		out += '\t%d' % instruction.word
 		if(i % 8 == 7):
       p()
   file.write(out)
+def dissassemble(word):
+  pass
 def findDataAddress(machine):
-		for i, instruction in enumerate(machine.instructions):
+		for i, instruction in enumerate(machine.memory.instructions):
+      instruction = Instruction(instruction)
 			if instruction['op'] == 0b100000 and instruction['func'] == 0b001101: #BREAK statement
 				return i + 1
-		return len(instructions)
+		return -1
